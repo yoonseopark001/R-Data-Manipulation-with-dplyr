6a8b44e38fc6778c4b8284a133a83e300a78d0d6
Yoonseo Park <yoonseopark001@gmail.com>
2019-12-30 15:46:02 +0900

    Update 01_Countries_dataset.R

--- 01_Countries_dataset.R
+++ 01_Countries_dataset.R
@@ -2,20 +2,64 @@
 # data: 2015 United States Census  
 
 select() 
-filter() 
 arrange() 
+filter() 
 mutate()
 
-countries
-glimpse(countries)
+=============================================================================
 
+## glimpse ##
+counties
+glimpse(counties)
+
+## select ##
 # select
-countries %>%   # %>%: pipe operator
-  select(state, country, population, unemployment)
+counties %>%   # %>%: pipe operator
+  select(state, county, population, unemployment)
   
 # creating a new table
-countries_selected <- countries %>%    
-  select(state, country, population, unemployment)
+counties_selected <- counties %>%    
+  select(state, county, population, unemployment)
 
-countries_selected 
+counties_selected 
+  
+=============================================================================
+
+## arrange ##
+counties_selected %>%
+  arrange(population)
+
+counties_selected %>%
+  arrange(desc(population)) # descending
+
+## filter ##
+counties_selected %>%
+  arrange(desc(population)) %>%
+  filter(state == "New York")
+
+counties_selected %>%
+  arrange(desc(population)) %>%
+  filter(unemployment < 6)
+
+counties_selected %>%
+  arrange(desc(population))  %>%
+  filter(state == "New York",
+        unemployment < 6)
+
+counties_selected %>%
+    filter(state == "California",
+        population > 1000000)
+=============================================================================
+
+counties_selected <- counties %>%
+  select(state, county, population, private_work, public_work, self_employed)
+
+counties_selected %>%
+  arrange(desc(public_work)) 
+
+=============================================================================
+counties_selected %>%
+  arrange(desc(private_work)) %>%
+  filter(state == "Texas",
+        population > 10000)
   
